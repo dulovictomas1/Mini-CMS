b3feb89329cf20a6fa9493b03bdf0eb282372fd5
Tomáš Dulovič <tomasdulovic@MacBook-Pro-uzivatela-Tomas.local>
2026-03-17 17:24:37 +0100

    zmeny

--- inc/edit-p.php
+++ inc/edit-p.php
@@ -6,6 +6,8 @@ $id = trim($_POST['id']);
 
 $ed_p = $database->update('products', ['Názov' => $_POST['name'], 'Popis' => $_POST['popis'], 'Cena' => $_POST['cena']], ['id' => $id]);
 
+//echo "Test ADD";
+
 if($ed_p) {
     header("Location:" . $base_url . "admin");
     die();
